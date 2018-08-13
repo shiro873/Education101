af128ad05e6345165af4cfb9b48c8b84aab1305d
ridwan shuvro <shuvro_r@hotmail.com>
2018-08-14 00:23:34 +0600

    flavor and splash update

--- app/src/main/java/projects/shiro/education101/activity/Splash/SplashActivity.java
+++ app/src/main/java/projects/shiro/education101/activity/Splash/SplashActivity.java
@@ -2,7 +2,9 @@ package projects.shiro.education101.activity.Splash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import projects.shiro.education101.BuildConfig;
 import projects.shiro.education101.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,6 +14,11 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
+        TextView txt = (TextView)findViewById(R.id.txt);
+        if(BuildConfig.FLAVOR.equals("freeVersion")){
+            txt.setText("free version");
+        }else {
+            txt.setText("Paid version");
+        }
     }
 }
